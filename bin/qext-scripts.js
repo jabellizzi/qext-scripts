7c4dd4c4c1b297c4c0ca82eb959159beb32301bf
John Bellizzi <jabellizzi406@gmail.com>
2017-12-28 20:54:11 -0500

    updated script module export

--- bin/qext-scripts.js
+++ bin/qext-scripts.js
@@ -6,13 +6,17 @@ var childProcess = require('child_process');
 var program = require('commander');
 var path = require('path');
 
+var Build = require(path.resolve(__dirname, '../scripts/build.js'));
+var Deploy = require(path.resolve(__dirname, '../scripts/deploy.js'));
+var Watch = require(path.resolve(__dirname, '../scripts/watch.js'));
+
 program
-  .version('0.1.11')
+  .version('0.2.0')
   .option('-b, --bundle', 'Bundle')
   .option('-d, --deploy', 'Deploy')
   .option('-w, --watch', 'Watch')
   .parse(process.argv);
 
-if(program.bundle) childProcess.fork(path.resolve(__dirname, '../scripts/build.js'));
-else if(program.deploy) childProcess.fork(path.resolve(__dirname, '../scripts/deploy.js'));
-else if(program.watch) childProcess.fork(path.resolve(__dirname, '../scripts/watch.js'));
+if(program.bundle) Build();
+else if(program.deploy) Deploy();
+else if(program.watch) Watch();
